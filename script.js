7ec9e95281d282eab87fb4a7a162b9b9d997018a
vikmeisterr <viktor.ivanovski11@gmail.com>
2023-10-25 16:02:07 +0200

    add/subtract/multiple/divide functions done

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log("Setup!");
+function add(num1, num2) {
+  return num1 + num2;
+}
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+let num1;
+let num2;
+let operator;
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      if (num2 == 0) {
+        return "ERROR!";
+      }
+      return divide(num1, num2);
+  }
+}
+console.log(operate("+", 1.3, 10));
