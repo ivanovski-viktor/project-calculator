8e179191a421df0d7b2f9216fcb51500127b6296
vikmeisterr <viktor.ivanovski11@gmail.com>
2023-11-07 01:02:39 +0100

    Calculator is working fine, I need to add size limiter for numbers

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
+// OPERATE FUNCTION
 let num1;
 let num2;
-let operator;
-
+let operator = "";
 function operate(operator, num1, num2) {
   switch (operator) {
     case "+":
@@ -25,10 +25,63 @@ function operate(operator, num1, num2) {
     case "*":
       return multiply(num1, num2);
     case "/":
-      if (num2 == 0) {
+      if (num2 === 0) {
         return "ERROR!";
       }
       return divide(num1, num2);
   }
 }
-console.log(operate("+", 1.3, 10));
+//CATCHING CLICK EVENTS
+let result = document.querySelector("#result");
+let btnsClass = document.querySelectorAll(".btns-class");
+let operatorClass = document.querySelectorAll(".operator-class");
+let solutionClass = document.querySelector(".solution-class");
+
+let clear = document.querySelector(".clear").addEventListener("click", () => {
+  result.textContent = "";
+  num1 = undefined;
+  num2 = undefined;
+  operator = undefined;
+});
+
+// ADDING CLICK FUNCTIONALITY TO BUTTONS
+btnsClass.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    result.textContent += btn.value;
+
+    if (!operator) {
+      if (!num1) {
+        num1 = btn.value;
+      } else {
+        num1 += btn.value;
+      }
+    } else {
+      if (!num2) {
+        num2 = btn.value;
+      } else {
+        num2 += btn.value;
+      }
+    }
+  });
+});
+
+operatorClass.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (!operator) {
+      result.textContent += btn.value;
+      operator = btn.value;
+    }
+  });
+});
+
+solutionClass.addEventListener("click", () => {
+  const calculatedResult = operate(
+    operator,
+    parseFloat(num1),
+    parseFloat(num2)
+  );
+  result.textContent = calculatedResult;
+  num1 = calculatedResult;
+  num2 = undefined;
+  operator = undefined;
+});
